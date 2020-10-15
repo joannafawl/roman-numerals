3b6a765819707573360136b018aa6b79e4e72e18
joannafawl <joanna.fawl@madetech.com>
2020-10-15 14:42:38 +0100

    Stub in for gets in first acceptance testsa

--- spec/acceptance_tests_spec.rb
+++ spec/acceptance_tests_spec.rb
@@ -2,16 +2,18 @@ require_relative '../lib/main'
 
 describe "RomanNumerals" do
     context "When the user starts the program" do
-        xit "displays a welcome message" do
+        it "displays a welcome message" do
             main = Main.new
             message = "Welcome to the Roman Numeral Converter!\n"
+            allow(STDIN).to receive(:gets).and_return("")
+            main.run
 
             expect { main.run }.to output(a_string_including(message)).to_stdout
         end
     end
 
     context "When the app is run" do
-        xit "displays a menu" do
+        it "displays a menu" do
             main = Main.new
             menu = "What would you like to do?\n\n"\
             "\t1) Learn more about Roman numerals\n"\
@@ -20,6 +22,9 @@ describe "RomanNumerals" do
             "\t4) Take the test\n"\
             "\t5) Quit\n\n"\
             "Please enter an option from 1-5.\n"
+
+            allow(STDIN).to receive(:gets).and_return("")
+            main.run
 
             expect { main.run }.to output(a_string_including(menu)).to_stdout
         end
