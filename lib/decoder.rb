b6cc0a027e3f9369b30484ea4aa9117c04baffd7
joannafawl <joanna.fawl@madetech.com>
2020-10-15 13:38:56 +0100

    Refactor tests for decoder function

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -1,14 +1,15 @@
 require_relative 'converter'
 
 class Decoder < Converter
-    
+
     def convert(numeral)
-        if numeral == "I"
-            return 1
-        elsif numeral == "II"
-            return 2
-        else
-            return 3
-        end
+        result = 0
+        chars = numeral.split("")
+        chars.each { |char| 
+            if char == "I"
+                result += 1
+            end
+        }
+        return result
     end
 end
