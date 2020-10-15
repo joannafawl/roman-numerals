9a2d4c4f793c5e05663502410a88f86eeb57d388
joannafawl <joanna.fawl@madetech.com>
2020-10-15 13:47:11 +0100

    Refactor decoder function to use @@CONVERSIONS hash

--- lib/converter.rb
+++ lib/converter.rb
@@ -18,6 +18,5 @@ class Converter
     }
 
     def convert(number)
-
     end 
 end
--- lib/decoder.rb
+++ lib/decoder.rb
@@ -5,14 +5,12 @@ class Decoder < Converter
     def convert(numeral)
         result = 0
         chars = numeral.split("")
-        chars.each { |char| 
-            if char == "X"
-                result += 10
-            elsif char == "V"
-                result += 5
-            elsif char == "I"
-                result += 1
-            end
+        chars.each { |char|
+            @@CONVERSIONS.each { |integer, numeral|
+                if char == numeral
+                    result += integer
+                end
+            }
         }
         return result
     end
