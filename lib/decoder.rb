b3d77e1cc0018c75fc81b657d42ec3a210ff9bb1
joannafawl <joanna.fawl@madetech.com>
2020-10-15 12:03:56 +0100

    Added parent class Converter for Decoder and Encoder

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -1,26 +1,12 @@
-class Decoder
+require_relative 'converter'
 
-    CONVERTER = {
-        1000 => "M",
-        900 => "CM",
-        500 => "D",
-        400 => "CD",
-        100 => "C",
-        90 => "XC",
-        50 => "L",
-        40 => "XL",
-        10 => "X",
-        9 => "IX",
-        5 => "V",
-        4 => "IV",
-        1 => "I"
-    }
+class Decoder < Converter
 
     def convert(number)
         result = ""
-        CONVERTER.each { |integer, numeral|
+        @@CONVERSIONS.each { |integer, numeral|
             quotient, remainder = number.divmod(integer)
-            result << CONVERTER[integer] * quotient
+            result << @@CONVERSIONS[integer] * quotient
             number = remainder
         }
         return result
