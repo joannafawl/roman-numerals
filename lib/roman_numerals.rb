781c085259bb41d0383d03b8624bc6e179576cdc
joannafawl <joanna.fawl@madetech.com>
2020-10-15 10:48:09 +0100

    Converter uses quotients and remainders to add to result for more complex numbers

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -12,16 +12,24 @@ class RomanNumerals
     CONVERTER = {
         20 => "XX",
         10 => "X",
-        6 => "VI",
+        #6 => "VI",
         5 => "V",
-        3 => "III",
-        2 => "II",
+        #3 => "III",
+        #2 => "II",
         1 => "I"
     }
 
     def converter(number)
         if CONVERTER.key?(number)
             return CONVERTER[number]
+        else
+            result = ""
+            CONVERTER.each { |integer, numeral|
+                quotient, remainder = number.divmod(integer)
+                result << CONVERTER[integer] * quotient
+                number = remainder
+            }
         end
+        return result
     end 
 end
